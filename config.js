a75f59a1e6c167eac935a0b7902d0ff5f62b840f
ELD-cyber <m31830850@gmail.com>
2025-04-29 21:11:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "B.m.b-Xmd~JPU2nSqJ#FjXBn445_-MziRPH41jtPB6mdtZv2FoA8Ct1t7VFD9k",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
